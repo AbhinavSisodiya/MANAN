6a2a36a3725a4f76c49b27fc03937f4cb46480f5
AbhinavSisodiya <77894051+AbhinavSisodiya@users.noreply.github.com>
2021-01-25 13:44:01 +0530

    Update common multiples new.cpp

--- common multiples new.cpp
+++ common multiples new.cpp
@@ -6,9 +6,8 @@ int maximum_of_array(int *ptr,int n){
 		if(*(ptr+i)>=max){
 			max=*(ptr+i);
 			}
-	return max;
 	}
-	
+	return max;
 }
 int main (){
 		int n,lcm=0;
@@ -23,12 +22,13 @@ int main (){
 	int limit,r;
 	cout<<"Enter the number till which you want to print the common multiples"<<endl;
 	cin>>limit;
-
+	
 	int maximum=maximum_of_array(arr,n);
+	
 	if(maximum>=limit){cout<<"Wait!!! Have some sense you are looking for a common multiple less than the minimum element XD";
 		return 0;
 	}
-	
+
 	for(int j=1;j<limit;j++){
 		r=0;        //final value of r will indicate whether
 					//the respective number is a common multiple or not
@@ -50,7 +50,7 @@ int main (){
 		}
 
 	}
-
+	
 	int size = limit/lcm;
 		if (lcm!=0){
 			int size = limit/lcm;
@@ -60,17 +60,14 @@ int main (){
 			cmarr[m]=lcm*(m+1);
 			}
 
-  			for(int b=0;b<size;b++) cout<<cmarr[b]<<" ";
+  			for(int b=0;b<size;b++) cout<<cmarr[b]<<endl;
 		}
 
 		else {cout<<"Sorry no common multiples exits till "<<limit<<" number"<<endl;}
-		
-		/*for(int m=0;m<size;m++){
-			cout<<lcm*(m+1)<<endl;
+		/*int a=lcm;
+		for(int m=0;m<size;m++){
+			lcm=a*(m+1);
+			cout<<lcm<<endl;
 		}*/
-
 		return 0;
 	}
-	
-
-
